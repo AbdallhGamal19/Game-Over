e8e7eae9de523d8ba68c3b280b9a6569c5f2498b
Abdallh <bj5710603@gmail.com>
2022-12-18 11:16:23 +0200

    add carousel

--- src/component/ItemDitails/ItemDitails.jsx
+++ src/component/ItemDitails/ItemDitails.jsx
@@ -37,8 +37,34 @@ export default function ItemDitails() {
                 <p className=''>{dataList.description}</p>
                 <h3 className='h4'>{dataList.title} Global Screenshots</h3>
                 {dataList.screenshots&&
+                  <>
+              <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src={dataList.screenshots[0].image}  className='w-100 mb-3 d-block' alt="" />
+                  </div>
+                  <div class="carousel-item">
+                    <img src={dataList.screenshots[1].image}  className='w-100 mb-3 d-block' alt="" />
+                  </div>
+                  <div class="carousel-item">
+                    <img src={dataList.screenshots[2].image}  className='w-100 mb-3 d-block' alt="" />
+                  </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+              </div>
+
+
+
+                  </>
                 
-                <img src={dataList.screenshots[0].image}  className='w-100 mb-3 d-block' alt="" />
+                
                 }
                 
                 <div className="points">
